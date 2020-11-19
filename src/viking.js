ed819884d9147e502d60ef4ceeef4a0af6ee967e
Katja Maasch <katja.maasch@gmail.com>
2020-11-19 12:07:19 +0100

    I stop working on it now

--- src/viking.js
+++ src/viking.js
@@ -1,47 +1,51 @@
 // Soldier
 class Soldier {
-  constructor(health, strength) {
-    this.health=health,
-    this.strength=strength,
+    constructor(health, strength) {
+      this.health = health;
+      this.strength = strength;
+      console.log(health, this.health)
+    }
 
-    this.attack= function() {
-        return this.strength;
-        }
-    this.receiveDamage= function(damage) {
-        this.health=health-damage;
-      }
-      }
+    attack() {
+      return this.strength;
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+    }
   }
 
 
 // Viking
 class Viking extends Soldier {
     constructor (name, health, strength) {
-        super (health, strength, attack, receiveDamage)
-        this.name=name,
-        this.receiveDamage=function(damage) {
-            this.health=health-damage;
-            if (health>=0) {
-                return "${this.name} has received ${this.damage} points of damage"
+        super (health, strength)
+        this.name=name
+    }
+        receiveDamage=function(damage) {
+            this.health=this.health-damage;
+            if (this.health>=0) {
+                return `${this.name} has received ${damage} points of damage`
             }
             else {
-                return "${this.name} has died in act of combat"
+                return `${this.name} has died in act of combat`
             }
         }
-        this.battleCry=function(){
+        battleCry=function(){
             return "Odin Owns You All!"
         }
     }
 }
 
+// whats the difference: in class viking i first put "this.receiveD..." and "this.battleCry" and everything inside the constructor
+
 // Saxon
 class Saxon extends Soldier {
-    this.receiveDamage= function (damage){
-        if (health>0) {
-            return "A Saxon has received ${this.damage} points of damage"
+    receiveDamage= function (damage){
+        if (this.health>0) {
+            return `A Saxon has received ${damage} points of damage`
         }
         else {
-            return "A Saxon has died in combat"
+            return `A Saxon has died in combat`
         }
     }
 }
@@ -51,16 +55,20 @@ class War {
     constructor () {
     this.vikingArmy=[],
     this.saxonArmy=[],
+    }
 
-    this.addViking= function (viking) {
+    /*should receive 1 argument (a Viking object); should add the received Viking to the army
+shouldn't return anything*/
+
+    addViking= function (viking) {
         this.vikingArmy.push(viking);
     }
 
-    this.addSaxon= function (saxon) {
+    addSaxon= function (saxon) {
         this.saxonArmy.push(saxon);
     }
     
-    this.vikingAttack= function () {
+    vikingAttack= function () {
         const saxon= saxonArmy[Math.floor(Math.random()*this.saxonArmy.length)];
         const viking= vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)];
         saxon.receiveDamage(viking.strength);
@@ -71,10 +79,10 @@ class War {
     return 
     }
 
-    this.saxonAttack= function () {
+    saxonAttack= function () {
 
     }
-    this.showStatus= function () {
+    showStatus= function () {
 
     }
 }
